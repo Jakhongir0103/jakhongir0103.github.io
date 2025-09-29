ba8ae5c0a5ef473879efd1472ae79d2b835578c9
Jakhongir0103 <68238593+Jakhongir0103@users.noreply.github.com>
2025-09-29 09:16:20 +0000

    Deploying to gh-pages from @ Jakhongir0103/jakhongir0103.github.io@501eb73da99181c9787354493fd50377ae412df8 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Jakhongir0103/new",
+    "data-repo": "Jakhongir0103/jakhongir0103.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
